9424a148d43b9fb2077bfd34d2736a85ce6e63e1
Ayesha Pathan <ap133385@gmail.com>
2024-07-15 08:11:11 -0500

    Update MoveZeroes.java

--- Array/MoveZeroes.java
+++ Array/MoveZeroes.java
@@ -3,19 +3,18 @@ public class MoveZeroes {
     public void moveZeroes(int[] nums) {
         int lastNonZeroFoundAt = 0;
 
-        // Move all the non-zero elements to the beginning of the array
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
                 nums[lastNonZeroFoundAt++] = nums[i];
             }
         }
 
-        // Fill the remaining array with zeros
+        
         for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
             nums[i] = 0;
         }
 
-        // Print the result
+        
         System.out.println(Arrays.toString(nums));
     }
 
